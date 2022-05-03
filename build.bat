c59244140ca90c97ca4d25e720d6e1927be226d3
Warlord56x <danika200151@gmail.com>
2022-05-03 20:36:45 +0200

    build script, copyright notice

---
+++ build.bat
@@ -0,0 +1,4 @@
+cd bin
+jar cfe0 HOMM_DUEL_v0.2.0.zip Main *
+move /Y HOMM_DUEL_v0.2.0.zip ..
+cd ..
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Core.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Core.println("Copyright (C) 2022  Török Dániel (aka Warlord56x)");
+        Core.main(args);
+    }
+}
